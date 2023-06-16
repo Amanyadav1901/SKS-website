415da0b324d47864149defd4de66970be707d73d
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-06-16 13:34:43 +0530

    updates 16-06-23

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#242582",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        primary: "skyblue",
+        secondary: "black",
+        tertiary: "darkblue",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
